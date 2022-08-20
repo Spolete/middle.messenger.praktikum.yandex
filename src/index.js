cc1a348000fea728fe8821356aacc076ba022c19
Spolete <talash10@mail.ru>
2022-08-20 16:38:27 +0300

    create page SignIn SignUp

--- src/index.js
+++ src/index.js
@@ -1,8 +1,16 @@
-import template from './index.hbs'
+import signIn from './pages/signIn/signIn.hbs';
+import signUp from './pages/signUp/signUp.hbs';
+import './pages/';
 import './components';
 
 window.addEventListener('DOMContentLoaded', () => {
   const app = document.querySelector('#app');
 
-  app.innerHTML = template({variable: 'Hello World!'})
+  if (window.location.pathname === '/signIn') {
+    app.innerHTML = signIn();
+  } else if (window.location.pathname === '/signUp') {
+    app.innerHTML = signUp();
+  } else {
+    app.innerHTML = signIn();
+  }
 })
