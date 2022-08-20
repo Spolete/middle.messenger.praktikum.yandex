b97e7efd1e5ad8fd5bb3961e5f00adc259f04adf
Spolete <talash10@mail.ru>
2022-08-20 16:38:27 +0300

    create page SignIn SignUp

--- src/index.js
+++ src/index.js
@@ -1,8 +1,19 @@
-import template from './index.hbs'
+import signIn from './pages/signIn/signIn.hbs';
+import signUp from './pages/signUp/signUp.hbs';
+import menuLinks from './components/menu/menu.hbs';
+import './pages/';
 import './components';
 
 window.addEventListener('DOMContentLoaded', () => {
   const app = document.querySelector('#app');
+  const menu = document.querySelector('#menu');
+  menu.innerHTML = menuLinks();
 
-  app.innerHTML = template({variable: 'Hello World!'})
+  if (window.location.pathname === '/signIn') {
+    app.innerHTML = signIn();
+  } else if (window.location.pathname === '/signUp') {
+    app.innerHTML = signUp();
+  } else {
+    app.innerHTML = signIn();
+  }
 })
