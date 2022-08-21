507025595f05eabf2fc705a01dc802bbf506dcca
Spolete <talash10@mail.ru>
2022-08-21 20:41:39 +0300

    add ui

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,4 +1,5 @@
 import './signIn';
 import './signUp';
-import './profile/modules/changeInfo'
-import './profile/modules/changePassword'
+import './profile/modules/changeInfo';
+import './profile/modules/changePassword';
+import './chat';
